1c50d7ebc7b64e0f1f386a33b74c73c4d8a48702
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-18 17:38:49 +0100

    Esercizio bonus completato

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <header>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <MyTitle></MyTitle>
+    </header>
+
+    <main>
+      <MyHero></MyHero>
+    </main>
+    
+  </div>
+</template>
+
+<script>
+import MyTitle from './components/MyTitle.vue'
+import MyHero from './components/MyHero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyTitle,
+    MyHero
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+}
+
+header img{
+  width: 70px;
+  margin-right: 50px;
+}
+
+
+
+</style>
---
+++ src/components/MyHero.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="jumbotron">
+            <div class="jumbotron-items">
+                <img src="../assets/vue-jumbo.png" alt="">
+                <h2>Standard Tooling for Vue.js Development </h2>
+                <button>Get Started</button> 
+            </div>
+            
+        </div>
+        
+    </div>   
+</template>
+
+<script>
+    export default {
+        name: 'MyHero',
+    }
+
+</script>
+
+<style scoped>
+.jumbotron{
+    width: 100%;
+    max-height: 600px;
+    background-color: #f8f8f8;
+    color: #435365;
+    display: flex;
+    justify-content: center;
+    
+}
+.jumbotron-items{
+    max-width: 1280px;
+    width: 100%;
+    position: relative;
+}
+img{
+    width: 100%;
+    object-fit: cover;
+    
+}
+h2{
+    position: absolute;
+    font-size: 2em;
+    left: 20px;
+    bottom: 50%;
+    transform: translateY(+50%);
+    width: 30%;
+}
+
+button{
+    position: absolute;
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #4db986;
+    color: #fff;
+    border: none;
+    border-radius: 10px;
+    padding: 10px;
+    cursor: pointer;
+}
+    
+    
+</style>
